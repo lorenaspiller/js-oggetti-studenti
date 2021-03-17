e0b1f72c646c0a46d3bb11eb7bd13ad03a60b883
Lorena Spiller <lorenaspiller@gmail.com>
2021-03-17 14:33:54 +0100

    parte 1 completa

--- js/script.js
+++ js/script.js
@@ -4,6 +4,19 @@ Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, c
 Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 */
 
+// 1. Definisco l'oggetto e le sue chiavi-valori
+var studente = {
+  nome: "Lorena",
+  cognome: "Spiller",
+  eta: 35,
+}
+
+// 2. Attraverso l'oggetto e stampo le sue proprietà (chiave + valore)
+for (var key in studente) {
+  console.log(key + " --> " + studente[key]);
+}
+
+
 /*
 Parte 2
 Creare un array di oggetti di studenti.
