f703e1317513a216f75289b8850b34358fe61f30
Lorena Spiller <lorenaspiller@gmail.com>
2021-03-17 14:27:46 +0100

    stadio iniziale esercizi e consegne

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+Parte 1
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+*/
+
+/*
+Parte 2
+Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+*/
+
+/*
+Parte 3
+Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+*/
